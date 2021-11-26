ccc308880bd4df20ea87ea75bf040a09f637e2d6
Muntaeyeon <monty420420@naver.com>
2021-11-26 20:54:48 +0900

    react_component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
-
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+import World from './component/World';
 function App() {
-  const name = "Tom";
-  const naver = {
-  name: "네이버",
-  url: "https://www.naver.com/"
-  };
+  
   return (
     <div className="App">
-       <h1
-         style={{
-           color: "#f0f",
-           backgroundColor: "green"
-         }}
-       >
-         Hello, {name},<p>{2+3}</p>
-       </h1>
-       <a href={naver.url}>{naver.name}</a>
+      <Hello />
+      <Welcome />
+      <World />
     </div>
   );
 }
---
+++ src/component/Hello.js
@@ -0,0 +1,10 @@
+// const Hello = () => {
+//<p>Hello<p>
+//
+//};
+//export default Hello
+
+export default function Hello() {
+    
+   return <h1>Hello</h1>
+}
