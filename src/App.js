2b5d81c9fdaef95ee7e0042635e3c7ec3fdde41f
Muntaeyeon <monty420420@naver.com>
2021-11-29 23:59:51 +0900

    react_css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Hello from './component/Hello';
 import Welcome from './component/Welcome';
 import World from './component/World';
+import styles from "./component/App.module.css";
 function App() {
   
   return (
     <div className="App">
-      <Hello />
       <Welcome />
       <World />
+      <Hello />
+
+      
+      <div className={styles.box}>App</div>
+      
+
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -4,7 +4,26 @@
 //};
 //export default Hello
 
+import styles from "./Hello.module.css";
+
 export default function Hello() {
     
-   return <h1>Hello</h1>
+   return (
+       <div>
+       <h1
+         style={
+             {
+                 color : "#f00",
+                 borderRight : "2px solid #000",
+                 marginBottom: "50px",
+                 opacity: 1
+             }
+         }
+       
+       >
+       Hello
+       </h1>
+       <div className={styles.box}>Hello</div>
+       </div>
+   )
 }
