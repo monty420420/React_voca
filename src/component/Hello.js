366c224b55803b23cbf23acdaa18b363afacb945
Muntaeyeon <monty420420@naver.com>
2021-12-03 23:13:08 +0900

    react_event

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -7,7 +7,19 @@
 import styles from "./Hello.module.css";
 
 export default function Hello() {
-    
+    function showName() {
+        console.log("Mike");
+    }
+
+    function showAge(age){
+        console.log(age);
+    }
+
+    function showText(txt) {
+        console.log(txt);
+    }
+
+
    return (
        <div>
        <h1
@@ -24,6 +36,16 @@ export default function Hello() {
        Hello
        </h1>
        <div className={styles.box}>Hello</div>
+
+       <button onClick={showName}>show name</button>
+       <button onClick={() =>{
+           showAge(10)
+       }}>show age
+       </button>
+       <input type="text" onChange={(e) => {
+         const txt = e.target.value;
+         showText(txt);
+       }} />
        </div>
    )
 }
