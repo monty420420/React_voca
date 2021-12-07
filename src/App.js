0eb4db9d7d1377f7c0ece02f318e35971a580115
Muntaeyeon <monty420420@naver.com>
2021-12-07 19:25:52 +0900

    react_map_dummyData

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
-import Hello from './component/Hello';
-import Welcome from './component/Welcome';
-import World from './component/World';
-import styles from "./component/App.module.css";
+import Header from './component/Header';
+import DayList from './component/DayList';
+import Day from './component/Day';
 function App() {
   
   return (
     <div className="App">
-      <Welcome />
-      <World />
-      <Hello age={10} />
-      <Hello age={20} />
-      <Hello age={310} />
-
-      
-      <div className={styles.box}>App</div>
-      
-
+      <Header />
+      <DayList /> 
+      <Day />
     </div>
   );
 }
